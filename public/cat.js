9830d711851a6771c44fa235ac318b632de8ec33
Takuma Hagiwara <takuma.hagiwara@dena.com>
2018-02-17 23:25:34 +0900

    なまいきなねこ追加

---
+++ public/cat.js
@@ -0,0 +1,78 @@
+// ローカル変数
+var isTalking = false;
+var catImage = document.createElement('img');
+var PATH = "images/cat_";
+var loadingCount = 0;
+var message_elm = document.getElementById('message');
+
+window.addEventListener('load', Init);
+
+// 猫の初期化
+function Init()
+{
+		var place = document.getElementById('cat-placeholder');
+		catImage.src = "images/cat_loading.png";
+		catImage.id = "cat";
+		//catImage.addEventListener("click",ChangeCatImage);
+		catImage.addEventListener("click",OnCatClicked);
+		place.insertBefore(catImage, place.firstChild);
+		requestAnimationFrame(Update);
+}
+
+function Update()
+{
+		requestAnimationFrame(Update);
+		if(!isTalking)
+		{
+			 loadingCount++;
+			 if(loadingCount > 40)
+			 {
+				 loadingCount = 0;
+			 }
+
+			 UpdateLoadingText(loadingCount);
+		}
+}
+
+function UpdateLoadingText(count)
+{
+		var text = ".";
+		for(i = 0; i < count/10; i++)
+		{
+				text += ".";
+		}
+		message_elm.innerHTML = text;
+}
+
+function ChangeCatImage()
+{
+	if(isTalking){
+		catImage.src = PATH+"loading.png";
+	}
+	else {
+		catImage.src = PATH+"talking.png";
+	}
+	isTalking = !isTalking;
+}
+
+function OnCatClicked()
+{
+	// firebaesからデータを取ってくる
+	var data = FetchMessageData();
+}
+
+function FetchMessageData()
+{
+	var database = firebase.database();
+	var message = [];
+	database.ref('/cat_message/message').once('value').then(function(snapshot){
+		snapshot.forEach(function(childSnapshot) {
+			message.push(childSnapshot.val());
+		});
+
+		// メッセージの抽選
+		var random = Math.floor( Math.random() * message.length );
+		message_elm.innerHTML = message[random];
+		ChangeCatImage();
+	});
+}
