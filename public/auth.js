030bcfdc4e483483435df1bf0c80857ec70e94f6
萩原拓真mac(OSX) <hagiwara@riec.tohoku.ac.jp>
2018-02-23 00:33:12 +0900

    cssのキャッシュをクリアする処理を追加

--- public/auth.js
+++ public/auth.js
@@ -2,6 +2,8 @@ var root = document.getElementById('button-container');
 var auth_button = document.getElementById('auth-button');
 var logout_button = document.getElementById('logout-button');
 
+CreateUserAccountForm();
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
      // ログアウトボタンを出す
@@ -26,4 +28,28 @@ function Logout()
   }).catch(function(error) {
     console.log("ログアウトエラー");
 });
+}
+
+function CreateUserAccountForm()
+{
+  // form作る
+  var form = document.createElement("div");
+  form.className = "search-form-text-box";
+  // テキストインプット作る
+  var nameField = document.createElement("input");
+  nameField.type = "text";
+  // ボタン作る
+  var submitButton = document.createElement("input");
+  submitButton.type = "submit";
+
+  // formに追加
+  form.appendChild(nameField);
+  form.appendChild(submitButton);
+
+  // formを追加
+  var container = document.getElementById("register");
+  container.appendChild(form);
+
+  var body = document.getElementById("container");
+  body.style.display = "none";
 }
